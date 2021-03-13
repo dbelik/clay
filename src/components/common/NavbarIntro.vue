30dd673e5824434db415341cc81302d8cc7f10aa
Denis Belik <denis.belik2014@gmail.com>
2021-03-13 17:27:56 +0200

    add search animation in home page

--- src/components/common/NavbarIntro.vue
+++ src/components/common/NavbarIntro.vue
@@ -1,12 +1,12 @@
 <template>
     <nav class="fixed top-0 left-0 w-screen bg-white z-50">
-        <content-container class="flex items-center justify-between py-24px">
-            <div class="flex items-center">
+        <content-container class="flex items-center justify-between md:justify-start py-24px">
+            <div class="flex items-center overflow-hidden whitespace-nowrap w-44">
                 <img class="h-icon mr-16px" src="/img/icons/clay.svg" alt="Clay" />
                 <h3 class="m-0">Clay Shop</h3>
             </div>
             
-            <ul class="hidden md:flex items-center justify-center">
+            <ul class="hidden md:flex items-center justify-center w-4/5">
                 <li class="mr-48px flex items-center">
                     <a class="inline-block" href="/"><img src="/img/icons/socials/instagram.svg" alt="Instagram" /></a>
                 </li>
@@ -18,12 +18,21 @@
                 </li>
             </ul>
 
-            <ul class="hidden md:flex">
-                <li class="mr-16px">
-                    <button class="flex items-center"><img class="h-icon" src="/img/icons/search.svg" alt="Search" /></button>
+            <ul class="hidden md:flex relative">
+                <li class="mr-16px flex items-center justify-start">
+                    <button @click="showSearch" class="flex items-center"><img class="h-icon" src="/img/icons/search.svg" alt="Search" /></button>
+                    <h3 class="w-0 h-0 overflow-hidden text-black m-0 whitespace-nowrap">Search something</h3>
                 </li>
                 <li class="ml-16px">
                     <router-link to="/"><img class="h-icon" src="/img/icons/cart.svg" alt="Cart" /></router-link>
+                </li>
+                <li class="absolute right-0 opacity-0 hidden">
+                    <button @click="closeSearch">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6.34314 6.34314L17.6568 17.6568" stroke="#EDA3B5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M17.6568 6.34314L6.34314 17.6568" stroke="#EDA3B5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </button>
                 </li>
             </ul>
 
@@ -34,8 +43,12 @@
                 </svg>
             </button>
         </content-container>
+
+        <content-container class="h-0 overflow-hidden md:block hidden">
+            <input class="text-4xl w-full" placeholder="Search" style="caret-color: #EDA3B5;" />
+        </content-container>
             
-        <content-container class="mt-32px hidden md:block">
+        <content-container class="mt-32px hidden md:block overflow-hidden">
             <ul class="flex items-center justify-center ">
                 <li class="mr-30px">
                     <h3 class="m-0">
@@ -161,7 +174,68 @@ export default {
             gsap.to(line2, { rotation: 0, duration: .1, transformOrigin: "center center", delay: .4 });
             gsap.to(line1, { y: 0, duration: .1, delay: .5 });
             gsap.to(line2, { y: 0, duration: .1, delay: .5 });
-        }
+        },
+        
+        showSearch(event) {
+            const button = event.currentTarget;
+            const li = button.parentNode;
+            const ul = button.parentNode.parentNode;
+            const searchTitle = button.parentNode.children[1];
+            const logo = button.parentNode.parentNode.parentNode.children[0];
+            const item1 = button.parentNode.parentNode.children[1];
+            const socLinks = button.parentNode.parentNode.parentNode.children[1];
+            const navlinks = button.parentNode.parentNode.parentNode.parentNode.children[2];
+            const input = button.parentNode.parentNode.parentNode.parentNode.children[1];
+            const cancel = button.parentNode.parentNode.children[2];
+
+            gsap.to(logo, { opacity: 0, duration: .2 });
+            gsap.to(item1, { opacity: 0, duration: .2 });
+            gsap.to(socLinks, { opacity: 0, duration: .2 });
+            gsap.to(navlinks, { color: "#ffffff", duration: .2});
+
+            gsap.to(logo, { width: 0, duration: .4, delay: .4 });
+            gsap.to(socLinks, { width: 0, duration: .4, delay: .4, margin: "0" });
+            gsap.to(li, { width: "100%", duration: 0, delay: .8 });
+            gsap.to(ul, { width: "100%", duration: 0, delay: .8 });
+            gsap.to(searchTitle, { marginLeft: "24px", duration: 0, delay: .8 });
+            gsap.to(searchTitle, { width: "100%", duration: .4, delay: .8 });
+            gsap.to(searchTitle, { height: "100%", duration: 0, delay: .8 });
+            gsap.to(input, { height: "90px", duration: .4, delay: .8 });
+            gsap.to(navlinks, { height: "0", marginTop: "9px", duration: .4, delay: .8 });
+            gsap.to(cancel, { display: "block", duration: 0, delay: .8 });
+            gsap.to(cancel, { opacity: 1, duration: .4, delay: .8 });
+        },
+
+        closeSearch(event) {
+            const button = event.currentTarget;
+            const li = button.parentNode.parentNode.children[0];
+            const ul = button.parentNode.parentNode;
+            const searchTitle = button.parentNode.parentNode.children[0].children[1];
+            const logo = button.parentNode.parentNode.parentNode.children[0];
+            const item1 = button.parentNode.parentNode.children[1];
+            const socLinks = button.parentNode.parentNode.parentNode.children[1];
+            const navlinks = button.parentNode.parentNode.parentNode.parentNode.children[2];
+            const input = button.parentNode.parentNode.parentNode.parentNode.children[1];
+            const cancel = button.parentNode;
+
+            gsap.to(cancel, { opacity: 0, duration: .4 });
+            gsap.to(cancel, { display: "block", duration: 0, delay: .4 });
+            gsap.to(searchTitle, { width: "0", duration: .4 });
+            gsap.to(searchTitle, { marginLeft: "0", duration: 0, delay: .4 });
+            gsap.to(searchTitle, { height: "0", duration: 0, delay: .4 });
+            gsap.to(input, { height: "0", duration: .4 });
+            gsap.to(navlinks, { height: "68px", marginTop: "32px", marginBottom: "0", duration: .4 });
+
+            gsap.to(navlinks, { color: "#1B2437", duration: .2, delay: .8 });
+            gsap.to(logo, { width: "11rem", duration: .4, delay: .4 });
+            gsap.to(socLinks, { width: "80%", duration: .4, delay: .4 });
+            gsap.to(li, { width: "auto", duration: .4, delay: .4 });
+            gsap.to(ul, { width: "auto", duration: .4, delay: .4 });
+
+            gsap.to(logo, { opacity: 1, duration: .2, delay: .8 });
+            gsap.to(socLinks, { opacity: 1, duration: .2, delay: .8 });
+            gsap.to(item1, { opacity: 1, duration: .2, delay: .8 });
+        },
     }
 }
 </script>
--- src/components/links/WhiteBorder.vue
+++ src/components/links/WhiteBorder.vue
@@ -1,5 +1,5 @@
 <template>
-    <router-link @mouseenter="interactStart" @mouseleave="interactStop" @focusin="interactStart" @focusout="interactStop" class="inline-block relative overflow-hidden border-2 text-white border-white font-semibold border-opacity-20 rounded-lg py-11px px-26px text-xl" :to="to">
+    <router-link @mouseenter="interactStart" @mouseleave="interactStop" @focusin="interactStart" @focusout="interactStop" class="inline-block relative overflow-hidden border-2 text-white border-white font-semibold border-opacity-20 rounded-lg py-2 px-5 xl:py-11px xl:px-32px text-xl" :to="to">
         <span class="absolute w-full h-full bg-white top-full left-0 z-0 text-pink flex items-center justify-center"><slot /></span>
         <span class="z-10"><slot /></span>
     </router-link>
