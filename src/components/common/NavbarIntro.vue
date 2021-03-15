4907e35828cc6522566477064b8f0f47b415e3eb
Denis Belik <denis.belik2014@gmail.com>
2021-03-15 17:04:33 +0200

    add slider to the home page (not responsive)

--- src/components/common/NavbarIntro.vue
+++ src/components/common/NavbarIntro.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="fixed top-0 left-0 w-screen bg-white z-50" style="height: 201px;">
+    <nav class="fixed top-0 left-0 w-screen bg-white z-50">
         <intro-content-container 
             class="flex items-center justify-between md:justify-start py-24px"
             style="max-width: 1480px"
@@ -242,3 +242,11 @@ export default {
     }
 }
 </script>
+
+<style>
+@media (min-width: 768px) {
+    nav {
+        height: 201px;
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -52,19 +52,42 @@
 
   <section>
     <intro-content-container
-      class="my-16px bg-white rounded-lg"
-      style="height: 347px"
+      class="my-16px rounded-lg"
     >
-      <!--<carousel :items-to-show="1.5" class="h-full">
-        <slide v-for="slide in 10" :key="slide" class="h-58">
-          <div class="h-full">123123</div>
-        </slide>
-        <template #addons>
-          <navigation />
-          <pagination />
-        </template>
-      </carousel>
-      -->
+      <splide :options="options">
+        <splide-slide>
+          <div class="w-full h-full flex">
+            <div class="w-1/4 h-full flex flex-col items-center justify-center">
+              <img src="/img/home/slider/slide_1/louis_vouiton.png" alt="Louis Vouiton" />
+              <h4 class="mb-4 mt-12 font-bold">Louis Vouiton</h4>
+            </div>
+
+            <div class="w-1/4 h-full flex flex-col items-center justify-center">
+              <img src="/img/home/slider/slide_1/dolce&gabbana.png" alt="Dolce&Gabbana" />
+              <h4 class="mb-6 mt-10 font-bold">Dolce&Gabbana</h4>
+            </div>
+
+            <div class="w-1/4 h-full flex flex-col items-center justify-center">
+              <img src="/img/home/slider/slide_1/gucci.png" alt="Gucci" />
+              <h4 class="mb-0 mt-16 font-bold">Gucci</h4>
+            </div>
+
+            <div class="w-1/4 h-full flex flex-col items-center justify-center">
+              <img src="/img/home/slider/slide_1/dries_van_noten.png" alt="Dries van Noten" />
+              <h4 class="mb-5 mt-12 font-bold">Dries van Noten</h4>
+            </div>
+          </div>
+        </splide-slide>
+        <splide-slide>
+          Slide 2
+        </splide-slide>
+        <splide-slide>
+          Slide 3
+        </splide-slide>
+        <splide-slide>
+          Slide 4
+        </splide-slide>
+      </splide>
     </intro-content-container>
   </section>
 
@@ -101,8 +124,8 @@
 </template>
 
 <script>
-// import 'vue3-carousel/dist/carousel.css';
-// import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+import { Splide, SplideSlide } from '@splidejs/vue-splide';
+import '@splidejs/splide/dist/css/themes/splide-skyblue.min.css';
 
 import IntroContentContainer from "../components/common/ContainerIntro.vue"
 
@@ -114,15 +137,23 @@ import Card from '../components/common/Card.vue'
 export default {
   name: 'Home',
   components: {
+    Splide,
+    SplideSlide,
     IntroContentContainer,
     WhiteBorderLink,
     WhiteFillLink,
     Card
-    // Carousel,
-    // Slide,
-    // Pagination,
-    // Navigation,
   },
+  data() {
+    return {
+        options: {
+          rewind : true,
+          width : 1480,
+          height: 347,
+          gap : '1rem',
+        },
+    }
+  }
 }
 </script>
 
@@ -135,27 +166,70 @@ export default {
     }
   }
 
-  .carousel__icon {
+  .splide {
+    background-color: white;
+    border-radius: 16px;
+  }
+
+  .splide__arrow svg {
+    fill: #D1D1D6;
+    width: 20px;
+  }
+
+  .splide__arrow svg:hover, .splide__arrow svg:focus {
     fill: #D1D1D6;
   }
 
-  .carousel__next {
-    background: transparent;
-    right: 40px;
-  }
-  .carousel__prev {
-    background: transparent;
-    left: 40px;
+  .splide__arrow--prev {
+    left: 3rem;
+    top: 44%;
   }
 
-  .carousel__pagination-button {
-    background: #767c87;
-    width: 8px;
-    height: 8px;
+  .splide__arrow--next {
+    right: 3rem;
+    top: 44%;
+  }
+
+  .splide__pagination {
+    margin-bottom: 23px;
+  }
+
+  .splide__slide {
+    padding: 0px 100px;
+  }
+
+  .splide__pagination > li {
+    display: flex;
+    align-items: center;
+  }
+
+  .splide__pagination__page {
+    width: 9px;
+    height: 9px;
+    background: #1B2437;
+    opacity: .6;
+    margin: 0px 6px;
+    position: relative;
+  }
+
+  .splide__pagination__page:focus, .splide__pagination__page:hover {
+    background: #1B2437;
+    opacity: 1;
+  }
+
+  .splide__pagination__page.is-active {
+    background: #1B2437;
+    opacity: 1;
+    transform: scale(1);
+  }
+
+  .splide__pagination__page.is-active::after {
+    content: '';
+    width: 200%;
+    height: 200%;
+    position: absolute;
+    transform: translate(-50%, -50%);
+    border: 1px solid rgba(27, 36, 55, .4);
     border-radius: 50%;
   }
-  .carousel__pagination-button--active {
-    background: #1b2437;
-  }
-  
 </style>
